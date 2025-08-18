5cfeb01d13bd45d5fe72d4ba6ea47c8a9b3d5c84
Thao Dang <46556701+thaodangspace@users.noreply.github.com>
2025-08-18 15:02:44 +0700

    refactor(server): move container handlers to internal package

--- server/main.go
+++ server/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+
+	"github.com/example/code-sandbox/internal/handlers"
 )
 
 func main() {
+	h, err := handlers.NewContainerHandler()
+	if err != nil {
+		log.Fatalf("failed to init container handler: %v", err)
+	}
+
 	r := chi.NewRouter()
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
+	})
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/containers", h.ListContainers)
+		r.Post("/containers", h.CreateContainer)
 	})
 
 	http.ListenAndServe(":8080", r)
